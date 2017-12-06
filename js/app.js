d8f65029ffddb9ea8bdf763778d23a9f48a9c163
CORAUD\ktroufleau <ktroufleau@cdh-it.com>
2017-12-06 18:14:04 +0100

    Front

--- js/app.js
+++ js/app.js
@@ -3,16 +3,19 @@ req.open('GET', 'https://overwatchleague.com/en-us/api/schedule', false);
 req.send(null);
 
 if (req.status !== 200) {
-     alert("Error");
+	alert("Error");
 }
 var myData = JSON.parse(req.responseText);
 var myData_nbStages = Object.keys(myData['data']['stages']).length;
 var myData_nbMatches = 0;
 var today = new Date();
+var currentStage = null;
 var myData_current = null;
 loop1:
 for (var i = 0; i < myData_nbStages; i++) {
 	myData_nbMatches = Object.keys(myData['data']['stages'][i]['matches']).length;
+	currentStage = myData['data']['stages'][i]['name'];
+
 	loop2:
 	for (var y = 0; y < myData_nbMatches; y++) {
 		myData_current = myData['data']['stages'][i]['matches'][y];
@@ -29,7 +32,8 @@ var logo1 = myData_current['competitors'][0]['logo'];
 var logo2 = myData_current['competitors'][1]['logo'];
 var MatchDate = dateFormat(myData_current['startDate'], "dd mmm yy h:MM TT");
 var teams = Team1 +" VS " + Team2;
-document.getElementById("startDate").innerHTML = MatchDate;
+$("#stage span").html(currentStage);
+$("#date span").html(MatchDate);
 document.getElementById('logo1').src = logo1;
 document.getElementById("teams").innerHTML = teams;
 document.getElementById('logo2').src = logo2;
