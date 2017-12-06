dcc5ce3559028b69d363647c2c55439fd7d35f40
CORAUD\ktroufleau <ktroufleau@cdh-it.com>
2017-12-06 11:06:06 +0100

    Live Notif: OK; Css and code reindent

--- js/background.js
+++ js/background.js
@@ -2,29 +2,33 @@ var notifPoped = null;
 var ajax_call = function() {
 	$.ajax({
 		type: "GET",
-		url: "https://api.twitch.tv/helix/streams?user_login=fantabobshow",
+		url: "https://api.twitch.tv/helix/streams?user_login=khyarn",
 		headers: {'Client-ID': '74r6k01pksoouztdb23pc8483p6uf1'},
 		dataType: 'json',
 		success: function(data){
-			chrome.extension.getBackgroundPage().console.log("dans le success" + notifPoped);
-			chrome.extension.getBackgroundPage().console.log('data length' + data.data.length);
 			if (data.data.length > 0 && notifPoped != true) {
+				chrome.browserAction.setBadgeText({text : "LIVE"});
+				chrome.browserAction.setBadgeBackgroundColor({color: "#2ded2d"});
 				var isOnline = data.data[0].type;
 				var streamTitle = data.data[0].title;
 				notifPoped = true;
-				var notification = chrome.notifications.create(
-					'Stream on',{   
-						type: 'basic',
-						requireInteraction: true,
-						iconUrl: 'img/gm.png', 
-						title: "Stream Live !", 
-						message: "Cheers love! The stream's here ! \n" + streamTitle
-					},
-
-					);
+				var notification = new Notification("Stream On!", {
+					icon: 'img/gm.png',
+					body: "Cheers love! The stream's here ! \n https://www.twitch.tv/khyarn" + streamTitle,
+					requireInteraction: true     
+				});
+				notification.onclick = function()
+				{
+					var URL = "https://www.twitch.tv/kittykathee";
+					window.open(URL, '_blank');
+					this.close();
+				}
 			}
+
 			else if (data.data.length == 0){
 				notifPoped = false;
+				chrome.browserAction.setBadgeText({text : ""});
+				chrome.browserAction.setBadgeBackgroundColor({color: ""});
 			}
 		},
 		error: function(resultat, statut, erreur){
