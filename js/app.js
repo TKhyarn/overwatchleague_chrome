4468b3d429f6936b005da54264dc5f4be8c47f80
CORAUD\ktroufleau <ktroufleau@cdh-it.com>
2017-12-07 15:46:22 +0100

    Design Ok + New api to find next match

--- js/app.js
+++ js/app.js
@@ -2,38 +2,54 @@ const req = new XMLHttpRequest();
 req.open('GET', 'https://overwatchleague.com/en-us/api/schedule', false);
 req.send(null);
 
-if (req.status !== 200) {
-	alert("Error");
-}
-var myData = JSON.parse(req.responseText);
-var myData_nbStages = Object.keys(myData['data']['stages']).length;
-var myData_nbMatches = 0;
-var today = new Date();
-var currentStage = null;
-var myData_current = null;
-loop1:
-for (var i = 0; i < myData_nbStages; i++) {
-	myData_nbMatches = Object.keys(myData['data']['stages'][i]['matches']).length;
-	currentStage = myData['data']['stages'][i]['name'];
+if (req.status === 200) {
 
-	loop2:
-	for (var y = 0; y < myData_nbMatches; y++) {
-		myData_current = myData['data']['stages'][i]['matches'][y];
-		if (new Date(myData_current['startDate']) > today) {
-			break loop1;
+	var myData = JSON.parse(req.responseText);
+	var myData_nbStages = Object.keys(myData['data']['stages']).length;
+	var myData_nbMatches = 0;
+	var today = new Date();
+	var currentStage = null;
+	var myData_current = null;
+	loop1:
+	for (var i = 0; i < myData_nbStages; i++) {
+		myData_nbMatches = Object.keys(myData['data']['stages'][i]['matches']).length;
+		currentStage = myData['data']['stages'][i]['name'];
+
+		loop2:
+		for (var y = 0; y < myData_nbMatches; y++) {
+			myData_current = myData['data']['stages'][i]['matches'][y];
+			if (new Date(myData_current['startDate']) > today) {
+				break loop1;
+			}
+
 		}
+	}
 
+	var Team1 = myData_current['competitors'][0]['name'];
+	var Team2 = myData_current['competitors'][1]['name'];
+	var logo1 = myData_current['competitors'][0]['logo'];
+	var logo2 = myData_current['competitors'][1]['logo'];
+	var MatchDate = dateFormat(myData_current['startDate'], "dd mmm yy h:MM TT");
+}
+
+else {
+	const req = new XMLHttpRequest();
+	req.open('GET', 'https://overwatchleague.com/en-us/api/live-match?locale=en-us', false);
+	req.send(null);
+	if (req.status === 200) {
+		var myData = JSON.parse(req.responseText);
+		var Team1 = myData['data']['liveMatch']['competitors'][0]['name'];
+		var Team2 = myData['data']['liveMatch']['competitors'][1]['name'];
+		var logo1 = myData['data']['liveMatch']['competitors'][0]['logo'];
+		var logo2 = myData['data']['liveMatch']['competitors'][1]['logo'];
+		var currentStage = myData['data']['liveMatch']['bracket']['stage']['title'];
+		var MatchDate = dateFormat(myData['data']['liveMatch']['startDate'], "dd mmm yy h:MM TT");
 	}
 }
 
-var Team1 = myData_current['competitors'][0]['name'];
-var Team2 = myData_current['competitors'][1]['name'];
-var logo1 = myData_current['competitors'][0]['logo'];
-var logo2 = myData_current['competitors'][1]['logo'];
-var MatchDate = dateFormat(myData_current['startDate'], "dd mmm yy h:MM TT");
-var teams = Team1 +" VS " + Team2;
 $("#stage span").html(currentStage);
 $("#date span").html(MatchDate);
-document.getElementById('logo1').src = logo1;
-document.getElementById("teams").innerHTML = teams;
-document.getElementById('logo2').src = logo2;
+$("#logoA").attr('src', logo1);
+$("#logoB").attr('src', logo2);
+$("#teamA span").html(Team1);
+$("#teamB span").html(Team2);
